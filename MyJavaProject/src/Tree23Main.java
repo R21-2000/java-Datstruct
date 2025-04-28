aa6d90fa35f582d03dcdaa9d028e54dbd35f97a9
Zefery <yusufaslam360@gmail.com>
2025-04-28 21:28:03 +0700

    referensi 23tree

--- MyJavaProject/src/Tree23Main.java
+++ MyJavaProject/src/Tree23Main.java
@@ -1,38 +1,23 @@
-/*
- * Modified from: http://www.java2s.com/ref/java/java-data-structures-234-tree.html
- * 
-*/
 import com.datastruct.nmTree;
 
 public class Tree23Main {
    public static void main(String[] args) {
-      nmTree theTree = new nmTree();
+      nmTree<Integer, String> theTree = new nmTree<>();
 
-      theTree.insert(50);
-      theTree.insert(40);
-      theTree.insert(60);
-      theTree.insert(30);
-      theTree.insert(70);
+      theTree.insert(50, "A");
+      theTree.insert(40, "B");
+      theTree.insert(60, "C");
+      theTree.insert(30, "D");
+      theTree.insert(70, "E");
 
-      theTree.insert(21);
-      theTree.insert(19);
-      theTree.insert(65);
-      theTree.insert(35);
-      theTree.insert(18);
-      theTree.insert(17);
-      theTree.insert(75);
-      //theTree.insert(73);
-      theTree.insert(80);
-      theTree.displayTree();
-      
-      int found = theTree.find(30);
-      if (found != -1)
-         System.out.println("40 ditemukan di tree");
-      else
-         System.out.println("40 tidak ditemukan di tree ");
-
-      theTree.delete(60);
-      System.out.println("2-3 tree after delete:");
+      theTree.insert(21, "F");
+      theTree.insert(19, "G");
+      theTree.insert(65, "H");
+      theTree.insert(35, "I");
+      theTree.insert(18, "J");
+      theTree.insert(17, "K");
+      theTree.insert(75, "L");
+      theTree.insert(80, "M");
       theTree.displayTree();
    }
 }
--- MyJavaProject/src/com/datastruct/nmTree.java
+++ MyJavaProject/src/com/datastruct/nmTree.java
@@ -1,112 +1,98 @@
 package com.datastruct;
-/*
- * Modified from: http://www.java2s.com/ref/java/java-data-structures-234-tree.html
- * 
-*/
 
-class DataItem {
-   private long dData;
+class DataItem<K extends Comparable<K>, V> {
+   private K key;
+   private V value;
 
-   public DataItem(long dd) {
-      dData = dd;
+   public DataItem(K key, V value) {
+      this.key = key;
+      this.value = value;
    }
 
-   public void setdData(long dData) {
-       this.dData = dData;
+   public K getKey() {
+      return key;
    }
 
-   public long getdData() {
-       return dData;
+   public void setKey(K key) {
+      this.key = key;
+   }
+
+   public V getValue() {
+      return value;
+   }
+
+   public void setValue(V value) {
+      this.value = value;
    }
 
    public void displayItem() {
-      System.out.print("/" + dData);
+      System.out.print("/" + key + ":" + value);
    }
 }
 
-class nmNode {
-   private static final int ORDER = 4; //2-3 Tree, ORDER = 4
+class nmNode<K extends Comparable<K>, V> {
+   private static final int ORDER = 4;
    private int numItems;
-   private nmNode parent;
-   private nmNode childArray[] = new nmNode[ORDER];
-   private DataItem itemArray[] = new DataItem[ORDER - 1];
-   
-   public void connectChild(int childNum, nmNode child) {
+   private nmNode<K, V> parent;
+   private nmNode<K, V>[] childArray = new nmNode[ORDER];
+   private DataItem<K, V>[] itemArray = new DataItem[ORDER - 1];
+
+   public void connectChild(int childNum, nmNode<K, V> child) {
       childArray[childNum] = child;
       if (child != null)
          child.parent = this;
    }
 
-   public nmNode disconnectChild(int childNum) {
-      nmNode tempnmNode = childArray[childNum];
+   public nmNode<K, V> disconnectChild(int childNum) {
+      nmNode<K, V> tempNode = childArray[childNum];
       childArray[childNum] = null;
-      return tempnmNode;
+      return tempNode;
    }
 
-   public nmNode getChild(int childNum) {
+   public nmNode<K, V> getChild(int childNum) {
       return childArray[childNum];
    }
 
-   public nmNode getParent() {
+   public nmNode<K, V> getParent() {
       return parent;
    }
 
    public boolean isLeaf() {
-      return (childArray[0] == null) ? true : false;
+      return (childArray[0] == null);
    }
 
    public int getNumItems() {
       return numItems;
    }
 
-   public DataItem getItem(int index) // get DataItem at index
-   {
+   public DataItem<K, V> getItem(int index) {
       return itemArray[index];
    }
 
    public boolean isFull() {
-      return (numItems == ORDER - 1) ? true : false;
+      return (numItems == ORDER - 1);
    }
 
-   public int findItem(long key)
-   {
-      for (int j = 0; j < ORDER - 1; j++) 
-      {
-         if (itemArray[j] == null) 
+   public int findItem(K key) {
+      for (int j = 0; j < ORDER - 1; j++) {
+         if (itemArray[j] == null)
             break;
-         else if (itemArray[j].getdData() == key)
+         else if (itemArray[j].getKey().compareTo(key) == 0)
             return j;
       }
       return -1;
    }
 
-   //re-arrange items in a node after disconnect a child
-   public void deleteItem(long key)
-   {
-      for (int j = 0; j < numItems; j++) 
-      {
-         if (itemArray[j] == null) 
-            break;
-         else if (itemArray[j].getdData() == key) {
-            for (int k = j; k < numItems; k++) {
-               itemArray[k] = itemArray[k+1];
-            }
-            --numItems;
-            break;
-         }
-      }
-   }
-
-   public int insertItem(DataItem newItem) {
+   public int insertItem(DataItem<K, V> newItem) {
       numItems++;
-      long newKey = newItem.getdData();
+      K newKey = newItem.getKey();
 
       for (int j = ORDER - 2; j >= 0; j--) {
          if (itemArray[j] == null)
             continue;
          else {
-            long itsKey = itemArray[j].getdData();
-            if (newKey < itsKey)
+            K itsKey = itemArray[j].getKey();
+            if (newKey.compareTo(itsKey) < 0)
                itemArray[j + 1] = itemArray[j];
             else {
                itemArray[j + 1] = newItem;
@@ -118,8 +104,8 @@ class nmNode {
       return 0;
    }
 
-   public DataItem removeItem() {
-      DataItem temp = itemArray[numItems - 1];
+   public DataItem<K, V> removeItem() {
+      DataItem<K, V> temp = itemArray[numItems - 1];
       itemArray[numItems - 1] = null;
       numItems--;
       return temp;
@@ -130,181 +116,69 @@ class nmNode {
          itemArray[j].displayItem();
       System.out.println("/");
    }
-
 }
 
-public class nmTree {
-   private nmNode root = new nmNode();
+public class nmTree<K extends Comparable<K>, V> {
+   private nmNode<K, V> root = new nmNode<>();
 
-   public int find(long key) {
-      nmNode curnmNode = root;
+   public int find(K key) {
+      nmNode<K, V> curNode = root;
       int childNumber;
       while (true) {
-         if ((childNumber = curnmNode.findItem(key)) != -1)
-            return childNumber; 
-         else if (curnmNode.isLeaf())
-            return -1; 
+         if ((childNumber = curNode.findItem(key)) != -1)
+            return childNumber;
+         else if (curNode.isLeaf())
+            return -1;
          else
-            curnmNode = getNextChild(curnmNode, key);
-      } 
-   }
-   public void insert(long dValue) {
-      nmNode curnmNode = root;
-      DataItem tempItem = new DataItem(dValue);
-      
-      while (true) {
-         if (curnmNode.isLeaf())
-            break;
-         else
-            curnmNode = getNextChild(curnmNode, dValue);
-      }
-      curnmNode.insertItem(tempItem);
-      // dilanggar dahulu max items per nmNode dari 2 menjadi 3 
-      if (curnmNode.isFull()) 
-      {
-        split(curnmNode); // split it
-        curnmNode = curnmNode.getParent();
-        // untuk setiap nmNode yang melanggar max items per nmNode
-        while(curnmNode.isFull()){
-            split(curnmNode); // split it
-            curnmNode = curnmNode.getParent();
-        }
+            curNode = getNextChild(curNode, key);
       }
    }
 
-   public nmNode adoptMerge(nmNode curNode, int index) {
-      boolean left = true;
-      nmNode parentNode = curNode.getParent();
-      int numItems = parentNode.getNumItems();
-      int j = 0; nmNode siblingNode = null;
-      int curPos = 0;
-      while(j < numItems && parentNode.getChild(j) != curNode) {
-         siblingNode = parentNode.getChild(j);
-         ++j;
-         curPos = j;
-      }
-      if(j == 0) {
-         siblingNode = parentNode.getChild(j+1);
-         left = false;
-      }
-      //select right sibling if it has more items than the left with single item
-      if(siblingNode.getNumItems() == 1 && j < numItems) {
-         if(parentNode.getChild(j+1).getNumItems() > 1) {
-            siblingNode = parentNode.getChild(j+1);
-            left = false;
-         }
-      }
-      //adopt
-      if(siblingNode.getNumItems() > 1) {
-         //left sibling
-         if(left) {
-            curNode.getItem(index).setdData(parentNode.getItem(j-1).getdData());
-            parentNode.getItem(j-1).setdData(siblingNode.getItem(siblingNode.getNumItems()-1).getdData());
-            if(curNode.isLeaf() == false) {
-               nmNode lastNode = siblingNode.disconnectChild(siblingNode.getNumItems());
-               nmNode tempNode = curNode.disconnectChild(0);
-               curNode.connectChild(1, tempNode);
-               curNode.connectChild(0, lastNode);
-            }
-            siblingNode.removeItem();
-         }
-         else {
-            //right sibling
-            curNode.getItem(index).setdData(parentNode.getItem(j).getdData());
-            parentNode.getItem(j).setdData(siblingNode.getItem(0).getdData());
-            if(curNode.isLeaf() == false) {
-               nmNode firstNode = siblingNode.disconnectChild(0);
-               nmNode tempNode = curNode.disconnectChild(curNode.getNumItems());
-               curNode.connectChild(0, tempNode);
-               curNode.connectChild(curNode.getNumItems(), firstNode);
-            }
-            siblingNode.deleteItem(siblingNode.getItem(0).getdData());
-         }
-      }
-      //merge
-      else {
-         if(parentNode.getNumItems() == 1) {
-            DataItem tempItem = new DataItem(parentNode.getItem(0).getdData());
-            siblingNode.insertItem(tempItem);
-            if(parentNode == root) {
-               return root;   
-            }
-            //merge or adopt upward
-            else {
-               curNode = parentNode;
-               return (adoptMerge(curNode, 0));
-            }
-         }
-         else {
-            DataItem tempItem = null;
-            if(curPos > 0) tempItem = new DataItem(parentNode.getItem(curPos-1).getdData());
-            else tempItem = new DataItem(parentNode.getItem(curPos).getdData());
-            siblingNode.insertItem(tempItem);
-            if(left) {
-               for(j=curPos; j < parentNode.getNumItems(); ++j) {
-                  nmNode tempNode = parentNode.disconnectChild(j+1);
-                  parentNode.connectChild(j, tempNode);
-               }
-               parentNode.deleteItem(tempItem.getdData());
-            }
-            else {
-               for(j=0; j < parentNode.getNumItems(); ++j) {
-                  nmNode tempNode = parentNode.disconnectChild(j+1);
-                  parentNode.connectChild(j, tempNode);
-               }
-               parentNode.deleteItem(tempItem.getdData());
-            }
-         }
-      }
-      return root;
-   }
-
-   //Delete a key from a leaf node in nmTree 
-   public void delete(long dValue) {
-      nmNode curNode = root;
+   public void insert(K key, V value) {
+      nmNode<K, V> curNode = root;
+      DataItem<K, V> tempItem = new DataItem<>(key, value);
 
       while (true) {
          if (curNode.isLeaf())
             break;
          else
-            curNode = getNextChild(curNode, dValue);
+            curNode = getNextChild(curNode, key);
       }
-      //delete leaf node
-      if(curNode.isLeaf()) { 
-         if(curNode.getNumItems() > 1) 
-            curNode.deleteItem(dValue);
-         else {
-            int index = curNode.findItem(dValue);
-            root = adoptMerge(curNode, index);//, -1);
-         }   
+      curNode.insertItem(tempItem);
+      if (curNode.isFull()) {
+         split(curNode);
+         curNode = curNode.getParent();
+         while (curNode.isFull()) {
+            split(curNode);
+            curNode = curNode.getParent();
+         }
       }
-      //delete internal node
-   }   
+   }
 
-   public void split(nmNode thisnmNode) {
-      DataItem itemB, itemC;
-      nmNode parent, child2, child3;
+   public void split(nmNode<K, V> thisNode) {
+      DataItem<K, V> itemB, itemC;
+      nmNode<K, V> parent, child2, child3;
       int itemIndex;
 
-      itemC = thisnmNode.removeItem();
-      itemB = thisnmNode.removeItem();
-      child2 = thisnmNode.disconnectChild(2);
-      child3 = thisnmNode.disconnectChild(3);
-      
-      nmNode newRight = new nmNode();
+      itemC = thisNode.removeItem();
+      itemB = thisNode.removeItem();
+      child2 = thisNode.disconnectChild(2);
+      child3 = thisNode.disconnectChild(3);
 
-      if (thisnmNode == root) {
-         root = new nmNode();
+      nmNode<K, V> newRight = new nmNode<>();
+
+      if (thisNode == root) {
+         root = new nmNode<>();
          parent = root;
-         root.connectChild(0, thisnmNode);
+         root.connectChild(0, thisNode);
       } else
-         parent = thisnmNode.getParent();
+         parent = thisNode.getParent();
 
       itemIndex = parent.insertItem(itemB);
       int n = parent.getNumItems();
 
       for (int j = n - 1; j > itemIndex; j--) {
-         nmNode temp = parent.disconnectChild(j);
+         nmNode<K, V> temp = parent.disconnectChild(j);
          parent.connectChild(j + 1, temp);
       }
       parent.connectChild(itemIndex + 1, newRight);
@@ -313,33 +187,31 @@ public class nmTree {
       newRight.connectChild(1, child3);
    }
 
-   public nmNode getNextChild(nmNode thenmNode, long theValue) {
+   public nmNode<K, V> getNextChild(nmNode<K, V> theNode, K theValue) {
       int j;
-
-      int numItems = thenmNode.getNumItems();
+      int numItems = theNode.getNumItems();
       for (j = 0; j < numItems; j++) {
-         if (theValue < thenmNode.getItem(j).getdData())
-            return thenmNode.getChild(j);
+         if (theValue.compareTo(theNode.getItem(j).getKey()) < 0)
+            return theNode.getChild(j);
       }
-      return thenmNode.getChild(j);
+      return theNode.getChild(j);
    }
 
    public void displayTree() {
       recDisplayTree(root, 0, 0);
    }
 
-   private void recDisplayTree(nmNode thisnmNode, int level, int childNumber) {
+   private void recDisplayTree(nmNode<K, V> thisNode, int level, int childNumber) {
       System.out.print("level=" + level + " child=" + childNumber + " ");
-      thisnmNode.displaynmNode(); 
+      thisNode.displaynmNode();
 
-      int numItems = thisnmNode.getNumItems();
+      int numItems = thisNode.getNumItems();
       for (int j = 0; j < numItems + 1; j++) {
-         nmNode nextnmNode = thisnmNode.getChild(j);
-         if (nextnmNode != null)
-            recDisplayTree(nextnmNode, level + 1, j);
+         nmNode<K, V> nextNode = thisNode.getChild(j);
+         if (nextNode != null)
+            recDisplayTree(nextNode, level + 1, j);
          else
             return;
       }
    }
-
 }
